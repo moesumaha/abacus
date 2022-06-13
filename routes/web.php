70c385d6fd9428dd4bcf4ce8041957dae40f1284
Moe Su Sandar <moesusandar@mahaawba.com>
2022-06-13 15:47:59 +0630

    Update route for post method

--- routes/web.php
+++ routes/web.php
@@ -18,4 +18,4 @@ use Illuminate\Support\Facades\Route;
 // });
 
 Route::get('/','MCIXController@index');
-Route::post('/','MCIXController@checkMCIX');
+Route::post('/check_mcix','MCIXController@checkMCIX');
